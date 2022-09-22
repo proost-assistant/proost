011ba7b00d7359f345b7b414544d08045870c7ef
Vincent Lafeychine <vincent.lafeychine@proton.me>
2022-09-22 16:41:47 +0200

    feat(term): Add early Term struct + Display trait

--- core/src/term.rs
+++ core/src/term.rs
@@ -16,7 +16,7 @@ impl Display for Term {
             Term::Prop => write!(f, "\u{02119}"),
             Term::Var(i) => write!(f, "{}", i),
             Term::Type(i) => write!(f, "\u{1D54B}({})", i),
-            Term::App(t1, t2) => write!(f, "({}) {}", t1, t2),
+            Term::App(t1, t2) => write!(f, "{} {}", t1, t2),
             Term::Abs(t1, t2) => write!(f, "\u{003BB}{} \u{02192} {}", t1, t2),
             Term::Prod(t1, t2) => write!(f, "\u{02200}{} \u{02192} {}", t1, t2),
         }
