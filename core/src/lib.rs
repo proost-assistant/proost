ea12c268170614a19eafcec74c8a38858790271d
aalbert <augustin.albert@bleu-azure.fr>
2022-09-26 10:16:34 +0200

    chore(core): format files

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #![feature(box_syntax)]
 
 mod command;
@@ -6,10 +5,3 @@ mod term;
 
 pub use command::Command;
 pub use term::Term;
-=======
-mod term;
-mod command;
-
-pub use term::Term;
-pub use command::Command;
->>>>>>> 69c81d9 (fix(core): add lib.rs)
