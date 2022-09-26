7f56047f549f716aa487df3a1bbb27897efb849c
aalbert <augustin.albert@bleu-azure.fr>
2022-09-26 10:04:55 +0200

    fix(core): add lib.rs

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #![feature(box_syntax)]
 
 mod command;
@@ -5,3 +6,10 @@ mod term;
 
 pub use command::Command;
 pub use term::Term;
+=======
+mod term;
+mod command;
+
+pub use term::Term;
+pub use command::Command;
+>>>>>>> 69c81d9 (fix(core): add lib.rs)
