9919231dde61aedcfc5589d8148cd97e24fd84c5
aalbert <augustin.albert@bleu-azure.fr>
2022-09-26 10:04:55 +0200

    fix(core): add lib.rs

---
+++ core/src/command.rs
@@ -0,0 +1,8 @@
+use crate::Term;
+
+#[derive(Clone, Debug)]
+pub enum Command {
+    Define(String,Term),
+    CheckType(Term,Term),
+    GetType(Term),
+}
--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -4,21 +4,26 @@ extern crate pest_derive;
 use pest::Parser;
 use pest::error::Error;
 use core::Term;
+use core::Command;
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 struct CommandParser;
 
-enum Command {
-    Define(String,Term),
-    CheckType(Term,Term),
-    GetType(Term,Term),
-}
-
 pub fn parse_term(file: &str) -> Result<Term, Error<Rule>> {
     let term = CommandParser::parse(Rule::term, file)?;
+
+    Ok(Term::Prop)
 }
 
-pub fn parse_command(file: &str) -> Result<Command, Error<Rule>> {}
+//TODO
+pub fn parse_command(file: &str) -> Result<Command, Error<Rule>> {
+    
+    Ok(Command::GetType(Term::Prop))
+}
 
-pub fn parse_file(file: &str) -> Result<Vec<Command>, Error<Rule>> {}
+//TODO
+pub fn parse_file(file: &str) -> Result<Vec<Command>, Error<Rule>> {
+
+    Ok(Vec::new())
+}
