beae2b241ba65991b6bc590c215a99b3f3034a5f
Vincent Lafeychine <vincent.lafeychine@proton.me>
2022-09-27 00:22:18 +0200

    feat(nix): Build Dockerfile from flake

--- flake.nix
+++ flake.nix
@@ -10,32 +10,52 @@
   outputs = { self, flake-utils, rust-overlay, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        rustPackages = pkgs.rust-bin.nightly.latest;
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit overlays system; };
+        rust = pkgs.rust-bin.nightly.latest;
       in rec {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "proost";
-          version = "0.1.0";
+        packages = {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "proost";
+            version = "0.1.0";
 
-          nativeBuildInputs = [ rustPackages.default ];
+            nativeBuildInputs = [ rust.minimal ];
 
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
 
-          meta = with pkgs.lib; {
-            description = "A simple proof assistant written in Rust";
-            homepage = "https://gitlab.crans.org/loutr/proost";
-            license = licenses.gpl3;
+            meta = with pkgs.lib; {
+              description = "A simple proof assistant written in Rust";
+              homepage = "https://gitlab.crans.org/loutr/proost";
+              license = licenses.gpl3;
+            };
+          };
+
+          docker-ci = let
+            rust-ci =
+              rust.minimal.override { extensions = [ "clippy" "rustfmt" ]; };
+          in pkgs.dockerTools.buildImage {
+            name = "proost-ci";
+
+            copyToRoot = pkgs.buildEnv {
+              name = "proost-dependencies";
+              paths = with pkgs; [ bash coreutils gcc openssh rust-ci ];
+            };
+
+            runAsRoot = "mkdir /tmp";
+
+            config = {
+              Env =
+                [ "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
+              Entrypoint = [ "${pkgs.bash}/bin/sh" "-c" ];
+            };
           };
         };
 
         devShells.default = pkgs.mkShell {
           name = "proost-dev";
-          packages = packages.default.nativeBuildInputs
-            ++ (with rustPackages; [ rustfmt clippy ]);
+          packages = rust.default.nativeBuildInputs
+            ++ (with rust; [ rustfmt clippy ]);
         };
       });
 }
