865855d6d325457a136a9ad0436c9104fbe8e68a
aalbert <augustin.albert@bleu-azure.fr>
2022-09-27 10:08:37 +0200

    feat(parser): add parser for terms, commands and files

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -85,13 +85,3 @@ pub fn parse_file(file: &str) -> Result<Vec<Command>, Box<Error<Rule>>> {
     }
     Ok(vec)
 }
-
-#[test]
-fn print() {
-    use std::fs;
-    let contents: &str =
-        &*fs::read_to_string("test.mdln").expect("Should have been able to read the file");
-
-    print!(" \n{:?}", parse_file(contents));
-    assert_eq!(1, 1);
-}
