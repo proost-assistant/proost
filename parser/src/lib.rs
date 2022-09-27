4f0b18d29a2656852b011c4cc5b773fee507347c
aalbert <augustin.albert@bleu-azure.fr>
2022-09-27 10:08:37 +0200

    feat(parser): add parser for terms, commands and files

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -4,24 +4,94 @@ extern crate pest_derive;
 use core::Command;
 use core::Term;
 use pest::error::Error;
+use pest::iterators::Pair;
 use pest::Parser;
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 struct CommandParser;
 
+fn build_term_from_expr(pair: Pair<Rule>) -> Term {
+    match pair.as_rule() {
+        Rule::Prop => Term::Prop,
+        Rule::Var => Term::Var(pair.into_inner().as_str().parse().unwrap()),
+        Rule::Type => Term::Type(pair.into_inner().as_str().parse().unwrap()),
+        Rule::App => {
+            let mut iter = pair.into_inner();
+            let t1 = build_term_from_expr(iter.next().unwrap());
+            let t2 = build_term_from_expr(iter.next().unwrap());
+            Term::App(Box::new(t1), Box::new(t2))
+        }
+        Rule::Abs => {
+            let mut iter = pair.into_inner();
+            let t1 = build_term_from_expr(iter.next().unwrap());
+            let t2 = build_term_from_expr(iter.next().unwrap());
+            Term::Abs(Box::new(t1), Box::new(t2))
+        }
+        Rule::Prod => {
+            let mut iter = pair.into_inner();
+            let t1 = build_term_from_expr(iter.next().unwrap());
+            let t2 = build_term_from_expr(iter.next().unwrap());
+            Term::Prod(Box::new(t1), Box::new(t2))
+        }
+        term => panic!("Unexpected term: {:?}", term),
+    }
+}
+
+fn build_command_from_expr(pair: Pair<Rule>) -> Command {
+    match pair.as_rule() {
+        Rule::GetType => {
+            let mut iter = pair.into_inner();
+            let t = build_term_from_expr(iter.next().unwrap());
+            Command::GetType(t)
+        }
+        Rule::CheckType => {
+            let mut iter = pair.into_inner();
+            let t1 = build_term_from_expr(iter.next().unwrap());
+            let t2 = build_term_from_expr(iter.next().unwrap());
+            Command::CheckType(t1, t2)
+        }
+        Rule::Define => {
+            let mut iter = pair.into_inner();
+            let s = iter.next().unwrap().as_str().to_string();
+            let t = build_term_from_expr(iter.next().unwrap());
+            Command::Define(s, t)
+        }
+        command => panic!("Unexpected command: {:?}", command),
+    }
+}
+
 pub fn parse_term(file: &str) -> Result<Term, Error<Rule>> {
-    let _term = CommandParser::parse(Rule::term, file)?;
-
-    Ok(Term::Prop)
+    let pair = CommandParser::parse(Rule::Term, file)?.next().unwrap();
+    Ok(build_term_from_expr(pair))
 }
 
-//TODO
-pub fn parse_command(_file: &str) -> Result<Command, Error<Rule>> {
-    Ok(Command::GetType(Term::Prop))
+pub fn parse_command(file: &str) -> Result<Command, Error<Rule>> {
+    let pair = CommandParser::parse(Rule::Command, file)?.next().unwrap();
+    Ok(build_command_from_expr(pair))
 }
 
-//TODO
-pub fn parse_file(_file: &str) -> Result<Vec<Command>, Error<Rule>> {
-    Ok(Vec::new())
+pub fn parse_file(file: &str) -> Result<Vec<Command>, Error<Rule>> {
+    let mut vec = Vec::new();
+    for pair in CommandParser::parse(Rule::File, file)?
+        .next()
+        .unwrap()
+        .into_inner()
+    {
+        match pair.as_rule() {
+            Rule::EOI => (),
+            _ => vec.push(build_command_from_expr(pair)),
+        }
+    }
+    Ok(vec)
+}
+
+#[test]
+fn print() {
+    use std::fs;
+    let contents: &str =
+        &*fs::read_to_string("test.mdln").expect("Should have been able to read the file");
+
+    print!(" \n{:?}", parse_file(contents));
+    assert_eq!(1, 1);
 }
