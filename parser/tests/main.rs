7ccd028a4d6b582c1ef6fe3b660c874874aaa674
aalbert <augustin.albert@bleu-azure.fr>
2022-09-27 10:08:37 +0200

    feat(parser): add parser for terms, commands and files

---
+++ parser/tests/main.rs
@@ -0,0 +1,11 @@
+#[test]
+fn parse() {
+    use std::fs;
+    let contents: &str =
+        &*fs::read_to_string("tests/test1.mdln").expect("Should have been able to read the file");
+
+    assert_eq!(
+        format!("{:?}", parser::parse_file(contents)),
+        "Ok([Define(\"Test\", Prop), GetType(Prop), Define(\"T2\", App(Prop, Prop))])"
+    );
+}
