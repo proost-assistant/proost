7a68fa0d1cba4ffa9f02dd8c123a694358304310
aalbert <augustin.albert@bleu-azure.fr>
2022-09-28 11:00:45 +0200

    feat(proost) : Basic loop with rustyline

--- proost/src/main.rs
+++ proost/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use rustyline::error::ReadlineError;
+use rustyline::{Editor, Result};
+
+fn main() -> Result<()> {
+    let mut rl = Editor::<()>::new()?;
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("Line: {}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    Ok(())
 }
