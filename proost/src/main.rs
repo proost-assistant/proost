e593984b8c527507cc032567de51f8d3ce37ed59
aalbert <augustin.albert@bleu-azure.fr>
2022-09-29 12:38:56 +0200

    feat(proost): Add Cargo dependencies & Command printing

--- proost/src/main.rs
+++ proost/src/main.rs
@@ -1,32 +1,37 @@
 use parser::parse_command;
 use rustyline::error::ReadlineError;
 use rustyline::{Editor, Result};
+use std::env;
 
 fn main() -> Result<()> {
-    let mut rl = Editor::<()>::new()?;
-    loop {
-        let readline = rl.readline(">> ");
-        match readline {
-            Ok(line) => {
-                rl.add_history_entry(line.as_str());
-                match parse_command(line.as_str()) {
-                    Ok(command) => println!("{}", command),
-                    Err(err) => println!("{}", *err),
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        //TODO
+        let file_path = &args[1];
+        println!("Fichier {}", file_path);
+        Ok(())
+    } else {
+        let mut rl = Editor::<()>::new()?;
+        loop {
+            let readline = rl.readline(">> ");
+            match readline {
+                Ok(line) => {
+                    rl.add_history_entry(line.as_str());
+                    match parse_command(line.as_str()) {
+                        Ok(command) => println!("{}", command),
+                        Err(err) => println!("{}", *err),
+                    }
+                }
+                Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
+                    println!("CTRL-C");
+                    break;
+                }
+                Err(err) => {
+                    println!("Error: {:?}", err);
+                    break;
                 }
             }
-            Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
-                break;
-            }
-            Err(ReadlineError::Eof) => {
-                println!("CTRL-D");
-                break;
-            }
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break;
-            }
         }
+        Ok(())
     }
-    Ok(())
 }
