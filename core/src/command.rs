78712ae2073b2b2fb0ac183d4e5e2b3e7d90a92a
aalbert <augustin.albert@bleu-azure.fr>
2022-10-04 17:24:46 +0200

    feat(parser): Add ClassicTerm with variables for easier user input

--- core/src/command.rs
+++ core/src/command.rs
@@ -1,8 +1,9 @@
-use crate::Term;
+use crate::ClassicTerm;
+//use std::fmt::{Display, Formatter};
 
 #[derive(Clone, Debug)]
 pub enum Command {
-    Define(String, Term),
-    CheckType(Term, Term),
-    GetType(Term),
+    Define(String, ClassicTerm),
+    CheckType(ClassicTerm, ClassicTerm),
+    GetType(ClassicTerm),
 }
