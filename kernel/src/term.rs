f9ecc76cd50e23d5378a634d3e263e0f20b74fa1
arthur-adjedj <arthur.adjedj@gmail.com>
2022-10-12 14:43:15 +0200

    feat : add `check` and `infer`

--- kernel/src/term.rs
+++ kernel/src/term.rs
@@ -42,7 +42,7 @@ impl Term {
         match self {
             App(box Abs(_, _, box t1), box t2) => t1.substitute(t2, 1),
             App(box t1, box t2) => App(box t1.beta_reduction(), box t2),
-            Abs(s, x, box t) => Abs(s,x, box t.beta_reduction()),
+            Abs(s, x, box t) => Abs(s, x, box t.beta_reduction()),
             _ => self,
         }
     }
@@ -52,7 +52,7 @@ impl Term {
             Var(i) if i > depth.into() => Var(i + offset.into()),
             App(box t1, box t2) => App(box t1.shift(offset, depth), box t2.shift(offset, depth)),
             Abs(s, t1, box t2) => Abs(s, t1, box t2.shift(offset, depth + 1)),
-            Prod(s,t1, box t2) => Prod(s,t1, box t2.shift(offset, depth + 1)),
+            Prod(s, t1, box t2) => Prod(s, t1, box t2.shift(offset, depth + 1)),
             _ => self,
         }
     }
@@ -66,8 +66,8 @@ impl Term {
                 box l.substitute(rhs.clone(), depth),
                 box r.substitute(rhs, depth),
             ),
-            Abs(s,t, term) => Abs(s,t, box term.substitute(rhs, depth + 1)),
-            Prod(s,t, term) => Prod(s,t, box term.substitute(rhs, depth + 1)),
+            Abs(s, t, term) => Abs(s, t, box term.substitute(rhs, depth + 1)),
+            Prod(s, t, term) => Prod(s, t, box term.substitute(rhs, depth + 1)),
             _ => self,
         }
     }
@@ -82,16 +82,25 @@ mod tests {
     #[test]
     fn simple_subst() {
         // λx.(λy.x y) x
-        let term = Abs("".into(),
+        let term = Abs(
+            "".into(),
             box Prop,
             box App(
-                box Abs("".into(),box Prop, box App(box Var(2.into()), box Var(1.into()))),
+                box Abs(
+                    "".into(),
+                    box Prop,
+                    box App(box Var(2.into()), box Var(1.into())),
+                ),
                 box Var(1.into()),
             ),
         );
 
         // λx.x x
-        let reduced = Abs("".into(),box Prop, box App(box Var(1.into()), box Var(1.into())));
+        let reduced = Abs(
+            "".into(),
+            box Prop,
+            box App(box Var(1.into()), box Var(1.into())),
+        );
 
         assert_eq!(term.beta_reduction(), reduced);
     }
@@ -100,19 +109,24 @@ mod tests {
     fn complex_subst() {
         // (λa.λb.λc.a (λd.λe.e (d b)) (λ_.c) (λd.d)) (λa.λb.a b)
         let term = App(
-            box Abs("".into(),
+            box Abs(
+                "".into(),
                 box Prop,
-                box Abs("".into(),
+                box Abs(
+                    "".into(),
                     box Prop,
-                    box Abs("".into(),
+                    box Abs(
+                        "".into(),
                         box Prop,
                         box App(
                             box App(
                                 box App(
                                     box Var(3.into()),
-                                    box Abs("".into(),
+                                    box Abs(
+                                        "".into(),
                                         box Prop,
-                                        box Abs("".into(),
+                                        box Abs(
+                                            "".into(),
                                             box Prop,
                                             box App(
                                                 box Var(1.into()),
@@ -121,33 +135,47 @@ mod tests {
                                         ),
                                     ),
                                 ),
-                                box Abs("".into(),box Prop, box Var(2.into())),
+                                box Abs("".into(), box Prop, box Var(2.into())),
                             ),
-                            box Abs("".into(),box Prop, box Var(1.into())),
+                            box Abs("".into(), box Prop, box Var(1.into())),
                         ),
                     ),
                 ),
             ),
-            box Abs("".into(),
+            box Abs(
+                "".into(),
                 box Prop,
-                box Abs("".into(),box Prop, box App(box Var(2.into()), box Var(1.into()))),
+                box Abs(
+                    "".into(),
+                    box Prop,
+                    box App(box Var(2.into()), box Var(1.into())),
+                ),
             ),
         );
 
-        let term_step_1 = Abs("".into(),
+        let term_step_1 = Abs(
+            "".into(),
             box Prop,
-            box Abs("".into(),
+            box Abs(
+                "".into(),
                 box Prop,
                 box App(
                     box App(
                         box App(
-                            box Abs("".into(),
+                            box Abs(
+                                "".into(),
                                 box Prop,
-                                box Abs("".into(),box Prop, box App(box Var(2.into()), box Var(1.into()))),
+                                box Abs(
+                                    "".into(),
+                                    box Prop,
+                                    box App(box Var(2.into()), box Var(1.into())),
+                                ),
                             ),
-                            box Abs("".into(),
+                            box Abs(
+                                "".into(),
                                 box Prop,
-                                box Abs("".into(),
+                                box Abs(
+                                    "".into(),
                                     box Prop,
                                     box App(
                                         box Var(1.into()),
@@ -156,25 +184,30 @@ mod tests {
                                 ),
                             ),
                         ),
-                        box Abs("".into(),box Prop, box Var(2.into())),
+                        box Abs("".into(), box Prop, box Var(2.into())),
                     ),
-                    box Abs("".into(),box Prop, box Var(1.into())),
+                    box Abs("".into(), box Prop, box Var(1.into())),
                 ),
             ),
         );
 
-        let term_step_2 = Abs("".into(),
+        let term_step_2 = Abs(
+            "".into(),
             box Prop,
-            box Abs("".into(),
+            box Abs(
+                "".into(),
                 box Prop,
                 box App(
                     box App(
-                        box Abs("".into(),
+                        box Abs(
+                            "".into(),
                             box Prop,
                             box App(
-                                box Abs("".into(),
+                                box Abs(
+                                    "".into(),
                                     box Prop,
-                                    box Abs("".into(),
+                                    box Abs(
+                                        "".into(),
                                         box Prop,
                                         box App(
                                             box Var(1.into()),
@@ -185,22 +218,26 @@ mod tests {
                                 box Var(1.into()),
                             ),
                         ),
-                        box Abs("".into(),box Prop, box Var(2.into())),
+                        box Abs("".into(), box Prop, box Var(2.into())),
                     ),
-                    box Abs("".into(),box Prop, box Var(1.into())),
+                    box Abs("".into(), box Prop, box Var(1.into())),
                 ),
             ),
         );
 
-        let term_step_3 = Abs("".into(),
+        let term_step_3 = Abs(
+            "".into(),
             box Prop,
-            box Abs("".into(),
+            box Abs(
+                "".into(),
                 box Prop,
                 box App(
                     box App(
-                        box Abs("".into(),
+                        box Abs(
+                            "".into(),
                             box Prop,
-                            box Abs("".into(),
+                            box Abs(
+                                "".into(),
                                 box Prop,
                                 box App(
                                     box Var(1.into()),
@@ -208,51 +245,71 @@ mod tests {
                                 ),
                             ),
                         ),
-                        box Abs("".into(),box Prop, box Var(2.into())),
+                        box Abs("".into(), box Prop, box Var(2.into())),
                     ),
-                    box Abs("".into(),box Prop, box Var(1.into())),
+                    box Abs("".into(), box Prop, box Var(1.into())),
                 ),
             ),
         );
 
-        let term_step_4 = Abs("".into(),
+        let term_step_4 = Abs(
+            "".into(),
             box Prop,
-            box Abs("".into(),
+            box Abs(
+                "".into(),
                 box Prop,
                 box App(
-                    box Abs("".into(),
+                    box Abs(
+                        "".into(),
                         box Prop,
                         box App(
                             box Var(1.into()),
-                            box App(box Abs("".into(),box Prop, box Var(3.into())), box Var(3.into())),
+                            box App(
+                                box Abs("".into(), box Prop, box Var(3.into())),
+                                box Var(3.into()),
+                            ),
                         ),
                     ),
-                    box Abs("".into(),box Prop, box Var(1.into())),
+                    box Abs("".into(), box Prop, box Var(1.into())),
                 ),
             ),
         );
 
-        let term_step_5 = Abs("".into(),
+        let term_step_5 = Abs(
+            "".into(),
             box Prop,
-            box Abs("".into(),
+            box Abs(
+                "".into(),
                 box Prop,
                 box App(
-                    box Abs("".into(),box Prop, box Var(1.into())),
-                    box App(box Abs("".into(),box Prop, box Var(2.into())), box Var(2.into())),
+                    box Abs("".into(), box Prop, box Var(1.into())),
+                    box App(
+                        box Abs("".into(), box Prop, box Var(2.into())),
+                        box Var(2.into()),
+                    ),
                 ),
             ),
         );
 
-        let term_step_6 = Abs("".into(),
+        let term_step_6 = Abs(
+            "".into(),
             box Prop,
-            box Abs("".into(),
+            box Abs(
+                "".into(),
                 box Prop,
-                box App(box Abs("".into(),box Prop, box Var(2.into())), box Var(2.into())),
+                box App(
+                    box Abs("".into(), box Prop, box Var(2.into())),
+                    box Var(2.into()),
+                ),
             ),
         );
 
         // λa.λb.b
-        let term_step_7 = Abs("".into(),box Prop, box Abs("".into(),box Prop, box Var(1.into())));
+        let term_step_7 = Abs(
+            "".into(),
+            box Prop,
+            box Abs("".into(), box Prop, box Var(1.into())),
+        );
 
         assert_eq!(term.beta_reduction(), term_step_1);
         assert_eq!(term_step_1.beta_reduction(), term_step_2);
