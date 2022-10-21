589e355cc875c74de29421d43c0f67a14b92b19b
arthur-adjedj <arthur.adjedj@gmail.com>
2022-10-21 12:03:35 +0200

    Chore :  add documentation

--- kernel/src/environment.rs
+++ kernel/src/environment.rs
@@ -2,6 +2,7 @@ use crate::term::Term;
 use derive_more::{From, Into};
 use std::collections::HashMap;
 
+/// Global Environment, contains the term and type of every definitions, denoted by their strings.
 #[derive(Clone, From, Into, Default)]
 pub struct Environment(HashMap<String, (Term, Term)>);
 
@@ -11,10 +12,12 @@ pub enum EnvError {
 }
 
 impl Environment {
+    /// Creates an empty environment.
     pub fn new() -> Environment {
         Default::default()
     }
 
+    /// Creates a new environment binding s with (t,ty)
     pub fn insert(
         self: Environment,
         s: String,
@@ -31,11 +34,14 @@ impl Environment {
         }
     }
 
+    /// Returns the term linked to a definition in a given environment.
     pub fn get_term(self: Environment, s: String) -> Option<Term> {
         <Environment as Into<HashMap<String, (Term, Term)>>>::into(self)
             .get(&s)
             .map(|(t, _)| t.clone())
     }
+
+    /// Returns the type linked to a definition in a given environment.
     pub fn get_type(self: Environment, s: String) -> Option<Term> {
         <Environment as Into<HashMap<String, (Term, Term)>>>::into(self)
             .get(&s)
