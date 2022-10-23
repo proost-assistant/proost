7ca83a2166e3fe4af31cd4b99c0bcb7ceaa5fed2
Lucas Tabary-Maujean <l.ta-ma@proton.me>
2022-10-23 18:06:23 +0200

    chore(cargo): add `im-rc` dependency for immutable structures

--- kernel/src/term.rs
+++ kernel/src/term.rs
@@ -1,40 +1,201 @@
-use crate::environment::Environment;
 use derive_more::{Add, Display, From, Into, Sub};
+
 use num_bigint::BigUint;
 
+use bumpalo::Bump;
+use im_rc::hashmap::HashMap as ImHashMap;
+use std::cell::OnceCell;
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
+
 #[derive(
-    Add, Copy, Clone, Debug, Default, Display, Eq, PartialEq, From, Into, Sub, PartialOrd, Ord,
+    Add, Copy, Clone, Debug, Default, Display, Eq, PartialEq, From, Into, Sub, PartialOrd, Ord, Hash,
 )]
 pub struct DeBruijnIndex(usize);
 
-#[derive(Add, Clone, Debug, Default, Display, Eq, From, Sub, PartialEq, PartialOrd, Ord)]
+#[derive(Add, Clone, Debug, Default, Display, Eq, From, Sub, PartialEq, PartialOrd, Ord, Hash)]
 pub struct UniverseLevel(BigUint);
 
-#[derive(Clone, Debug, Display, Eq, PartialEq)]
-pub enum Term {
-    #[display(fmt = "{}", _0)]
-    Var(DeBruijnIndex),
+pub struct Arena<'arena> {
+    alloc: Bump,
 
-    #[display(fmt = "{}", _0)]
-    Const(String),
-
-    #[display(fmt = "Prop")]
-    Prop,
-
-    #[display(fmt = "Type {}", _0)]
-    Type(UniverseLevel),
-
-    #[display(fmt = "{} {}", _0, _1)]
-    App(Box<Term>, Box<Term>),
-
-    #[display(fmt = "\u{003BB} {} \u{02192} {}", _0, _1)]
-    Abs(Box<Term>, Box<Term>),
-
-    #[display(fmt = "\u{03A0} {} \u{02192} {}", _0, _1)]
-    Prod(Box<Term>, Box<Term>),
+    hashcons: HashSet<&'arena Node<'arena>>,
+    named_terms: HashMap<&'arena str, Term<'arena>>,
 }
 
-use Term::*;
+#[derive(Clone, Copy, Eq, Debug)]
+pub struct Term<'arena>(&'arena Node<'arena>);
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Node<'arena> {
+    payload: Payload<'arena>,
+    // free_vars: an efficient type to measure whether a term is closed
+    // this can be added in future iterations
+    head_normal_form: OnceCell<Term<'arena>>,
+    type_: OnceCell<Term<'arena>>,
+}
+
+// type BinTermHashMap<'arena> = HashMap<(Term<'arena>, Term<'arena>), Term<'arena>>;
+// binary maps are not used so far, because, upon building terms, no particular optimisation is
+// done. On the other hand, if it is decided that only WHNF terms may exist in the arena, there can
+// be an AppMap (only App is relevant, as Abs and Prod preserve WHNF).
+//
+// We also need a substitution hasmap, (body, level_of_substitution, Term_to_incorporate)
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub enum Payload<'arena> {
+    Var(DeBruijnIndex, Term<'arena>),
+    Prop,
+    Type(UniverseLevel),
+    App(Term<'arena>, Term<'arena>),
+    Abs(Term<'arena>, Term<'arena>),
+    Prod(Term<'arena>, Term<'arena>),
+}
+
+use Payload::*;
+
+/// (TODO PRECISE DOCUMENTATION) make use of unicity invariant to speed up equality test
+impl<'arena> PartialEq<Term<'arena>> for Term<'arena> {
+    fn eq(&self, x: &Term<'arena>) -> bool {
+        std::ptr::eq(self.0, x.0)
+    }
+}
+
+/// (TODO PRECISE DOCUMENTATION) make use of unicity invariant to speed up equality test
+impl<'arena> Hash for Term<'arena> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        std::ptr::hash(self.0, state)
+    }
+}
+
+/// (TODO PRECISE DOCUMENTATION) Only the payload matters and caracterises the value. Changing
+/// OnceCells is *guaranteed* to have no impact on that.
+impl<'arena> Hash for Node<'arena> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.payload.hash(state);
+    }
+}
+
+type Environment<'arena> = ImHashMap<&'arena str, (DeBruijnIndex, Term<'arena>)>;
+trait ExternTermGenerator<'arena> = FnOnce(DeBruijnIndex, &Environment<'arena>) -> Term<'arena>;
+
+impl<'arena> Arena<'arena> {
+    /// (TODO DOC.) Allocate a new memory arena.
+    fn new() -> Self {
+        Arena {
+            alloc: Bump::new(),
+
+            hashcons: HashSet::new(),
+            named_terms: HashMap::new(),
+        }
+    }
+
+    /// (TODO DOC.) there are concurrent designs here: either hashcons take a Node and functions
+    /// which use it immediatly intend to compute the type and/or WHNF of the term, or this is
+    /// postponed and computed lazily. This iteration chooses the second approach.
+    fn hashcons(&mut self, n: Payload<'arena>) -> Term<'arena> {
+        let nn = Node {
+            payload: n,
+            head_normal_form: OnceCell::new(),
+            type_: OnceCell::new(),
+        };
+        match self.hashcons.get(&nn) {
+            Some(addr) => Term(addr),
+            None => {
+                let addr = &*Bump::alloc(&self.alloc, nn);
+                self.hashcons.insert(addr);
+                Term(addr)
+            }
+        }
+    }
+
+    pub fn var(&mut self, index: DeBruijnIndex, type_: Term<'arena>) -> Term<'arena> {
+        self.hashcons(Var(index, type_))
+    }
+
+    pub fn prop(&mut self) -> Term<'arena> {
+        self.hashcons(Prop)
+    }
+
+    pub fn type_(&mut self, level: UniverseLevel) -> Term<'arena> {
+        self.hashcons(Type(level))
+    }
+
+    pub fn app(&mut self, u1: Term<'arena>, u2: Term<'arena>) -> Term<'arena> {
+        self.hashcons(App(u1, u2))
+    }
+
+    pub fn abs(&mut self, arg_type: Term<'arena>, u: Term<'arena>) -> Term<'arena> {
+        self.hashcons(Abs(arg_type, u))
+    }
+
+    pub fn prod(&mut self, arg_type: Term<'arena>, u: Term<'arena>) -> Term<'arena> {
+        self.hashcons(Prod(arg_type, u))
+    }
+
+    /// These functions are available publicly, to the attention of the parser. They manipulate
+    /// objects with a type morally equal to (Env -> Term), where Env is a working environment used
+    /// in term construction from the parser.
+    /// This is done as a way to elengantly keep the logic encapsulated in the kernel, but let the
+    /// parser itself explore the term.
+
+    pub fn extern_var<'a>(&'arena mut self, name: &'arena str) -> impl ExternTermGenerator<'arena> {
+        move |depth, env: &Environment<'arena>| match env.get(&name) {
+            Some((bind_depth, term)) => self.var(depth - *bind_depth, *term),
+            None => panic!("change this for a proper error"),
+        }
+        env.get(&name).map(|(bind_depth, term)| Ok(self.var(depth - *bind_depth, *term))).
+    }
+
+    pub fn extern_prop(&'arena mut self) -> impl ExternTermGenerator<'arena> {
+        |depth, env: &Environment<'arena>| self.prop()
+    }
+
+    pub fn extern_type(&'arena mut self, level: UniverseLevel) -> impl ExternTermGenerator<'arena> {
+        |depth, env: &Environment<'arena>| self.type_(level)
+    }
+
+    pub fn extern_app<F1: ExternTermGenerator<'arena>, F2: ExternTermGenerator<'arena>>(
+        &'arena mut self,
+        u1: F1,
+        u2: F2,
+    ) -> impl ExternTermGenerator<'arena> {
+        |depth, env: &Environment<'arena>| self.app(u1(depth, env), u2(depth, env))
+    }
+
+    pub fn extern_abs<F1: ExternTermGenerator<'arena>, F2: ExternTermGenerator<'arena>>(
+        &'arena mut self,
+        name: &'arena str,
+        arg_type: F1,
+        body: F2,
+    ) -> impl ExternTermGenerator<'arena> {
+        move |depth, env: &Environment<'arena>| {
+            let arg_type = arg_type(depth, env);
+            let env = env.update(name, (depth + DeBruijnIndex(1), arg_type));
+            self.abs(arg_type, body(depth, &env))
+        }
+    }
+
+    pub fn extern_prod<F1: ExternTermGenerator<'arena>, F2: ExternTermGenerator<'arena>>(
+        &'arena mut self,
+        name: &'arena str,
+        arg_type: F1,
+        body: F2,
+    ) -> impl ExternTermGenerator<'arena> {
+        move |depth, env: &Environment<'arena>| {
+            let arg_type = arg_type(depth, env);
+            let env = env.update(name, (depth + DeBruijnIndex(1), arg_type));
+            self.prod(arg_type, body(depth, &env))
+        }
+    }
+
+    pub fn extern_gen<F: ExternTermGenerator<'arena>>(f: F) -> Term<'arena> {
+        f(DeBruijnIndex(0), &ImHashMap::new())
+    }
+}
+
+/*
+}
 
 impl Term {
     /// Apply one step of β-reduction, using leftmost outermost evaluation strategy.
@@ -347,3 +508,4 @@ mod tests {
         assert_eq!(term.beta_reduction(&Environment::new()), reduced);
     }
 }
+*/
