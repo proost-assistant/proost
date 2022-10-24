a7d9a5f0b27f98416f9e50e936dbc4fa385935c8
v-lafeychine <vincent.lafeychine@proton.me>
2022-10-24 07:41:49 +0200

    Resolve "Coverage: Artifacts and branches" ✨️ Closes #25 🐔️👍️ Approved-by: loutr <loutr@crans.org>
    🦀️🍰🦀️🍰🦀️🍰
    
    * fix(flake): Correctly interlink inputs
    
    * chore: Add coverage folder into gitignore
    
    * feat(ci): Output only branches in CI-view
    
    * feat(nix): Add coverage command with devshell
    
    * feat(ci, nix): Branches coverage

---
+++ .gitlab-cov.xsl
@@ -0,0 +1,26 @@
+<?xml version="1.0"?>
+
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:output method="xml"/>
+
+    <xsl:template match="node() | @*">
+        <xsl:copy>
+            <xsl:apply-templates select="node() | @*"/>
+        </xsl:copy>
+    </xsl:template>
+
+    <xsl:template match="line">
+        <xsl:copy>
+            <xsl:apply-templates select="@*"/>
+            <xsl:attribute name="hits">
+                <xsl:choose>
+                    <xsl:when test="conditions/condition[@coverage='0']">0</xsl:when>
+                    <xsl:when test="@hits='0'">0</xsl:when>
+                    <xsl:otherwise>1</xsl:otherwise>
+                </xsl:choose>
+            </xsl:attribute>
+            <xsl:apply-templates select="node()"/>
+        </xsl:copy>
+    </xsl:template>
+
+</xsl:stylesheet>
--- flake.nix
+++ flake.nix
@@ -3,14 +3,27 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = { self, flake-utils, rust-overlay, nixpkgs }:
+  outputs = { self, devshell, flake-utils, rust-overlay, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [ devshell.overlay (import rust-overlay) ];
         pkgs = import nixpkgs { inherit overlays system; };
         rust = pkgs.rust-bin.nightly.latest;
       in rec {
@@ -40,7 +53,7 @@
 
             copyToRoot = pkgs.buildEnv {
               name = "proost-dependencies";
-              paths = (with pkgs; [ coreutils gcc gnugrep gnused grcov openssh rust-ci ])
+              paths = (with pkgs; [ coreutils findutils gcc gnugrep gnused grcov lcov libxslt openssh rsync rust-ci ])
                 ++ (with pkgs.dockerTools; [ binSh caCertificates fakeNss ]);
               pathsToLink = [ "/bin" "/etc" ];
             };
@@ -49,9 +62,30 @@
           };
         };
 
-        devShells.default = pkgs.mkShell {
-          name = "proost-dev";
-          packages = [ (rust.default.override { extensions = [ "rust-src" "rust-analyzer" ]; }) ];
+        devShell = let
+          rust-dev = rust.default.override { extensions = [ "rust-src" "rust-analyzer" ]; };
+        in pkgs.devshell.mkShell {
+          name = "proost";
+
+          commands = [{
+            name = "coverage";
+            command = let 
+              excl_br_regexp = "#\\[|unreachable!()|assert(_eq)?!";
+              excl_regexp = "//!|///|#\\[|use";
+              env = "CARGO_INCREMENTAL=0"
+                  + " RUSTFLAGS=\"-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort\""
+                  + " RUSTDOCFLAGS=\"-Cpanic=abort\"";
+            in ''
+              ${env} cargo test
+              grcov . -s . -b ./target/debug/ --branch --ignore '*cargo*' --ignore-not-existing \
+                  --excl-line "${excl_regexp}" --excl-br-line "${excl_br_regexp}" -o ./target/coverage.lcov --log /dev/null || true
+              find target \( -name "*.gcda" -or -name "*.gcno" \) -delete
+              genhtml --branch --no-function-coverage --precision 2 target/coverage.lcov -o coverage
+            '';
+            help = "Launch tests and generate HTML coverage website";
+          }];
+
+          packages = with pkgs; [ grcov lcov rust-dev ];
         };
       });
 }
