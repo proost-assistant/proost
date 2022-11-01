328982eb0a7bf4b9b57c42de8a3b3f74d8bce79a
aalbert <augustin.albert@bleu-azure.fr>
2022-11-01 15:16:45 +0100

    Resolve " tests for parser" ✨️ Closes #8 🐔️👍️ Approved-by: v-lafeychine <vincent.lafeychine@proton.me> Approved-by: belazy <aarthuur01@gmail.com>
    🦀️🍰🦀️🍰🦀️🍰
    
    * feat(parser): resolve threads
    
    * Apply 1 suggestion(s) to 1 file(s)
    
    * feat(parser): resolve threads
    
    * Apply 1 suggestion(s) to 1 file(s)
    
    * feat(parser): more coverage
    
    * feat(parser): add one more test
    
    * feat(parser): add test for error conversion
    
    * feat(parser): more tests
    
    * feat(parser): more tests
    
    * feat(parser): more tests
    
    * feat(parser): first test

--- parser/src/parser.rs
+++ parser/src/parser.rs
@@ -3,20 +3,20 @@ use kernel::{Command, KernelError, Loc, Term};
 use pest::error::{Error, LineColLocation};
 use pest::iterators::Pair;
 use pest::{Parser, Span};
-use std::cmp::Ordering;
 use std::collections::VecDeque;
 
 #[derive(Parser)]
 #[grammar = "term.pest"]
 struct CommandParser;
 
-// convert pest location to kernel location
+/// convert pest locations to kernel locations
 fn convert_span(span: Span) -> Loc {
     let (x1, y1) = span.start_pos().line_col();
     let (x2, y2) = span.end_pos().line_col();
     Loc::new(x1, y1, x2, y2)
 }
 
+/// build terms from errorless pest's output
 fn build_term_from_expr(pair: Pair<Rule>, known_vars: &mut VecDeque<String>) -> Term {
     // location to be used in a future version
     let _loc = convert_span(pair.as_span());
@@ -44,11 +44,11 @@ fn build_term_from_expr(pair: Pair<Rule>, known_vars: &mut VecDeque<String>) -> 
             let pair = iter.next_back().unwrap();
             let mut terms = Vec::new();
             for pair in iter {
-                let mut iter = pair.into_inner().rev();
-                let t = build_term_from_expr(iter.next().unwrap(), known_vars);
-                for pair in iter.rev() {
+                let mut iter = pair.into_inner();
+                let old_pair = iter.next_back().unwrap();
+                for pair in iter {
+                    terms.push(build_term_from_expr(old_pair.clone(), known_vars));
                     known_vars.push_front(pair.as_str().to_string());
-                    terms.push(t.clone());
                 }
             }
             let t = build_term_from_expr(pair, known_vars);
@@ -62,11 +62,11 @@ fn build_term_from_expr(pair: Pair<Rule>, known_vars: &mut VecDeque<String>) -> 
             let pair = iter.next_back().unwrap();
             let mut terms = Vec::new();
             for pair in iter {
-                let mut iter = pair.into_inner().rev();
-                let t = build_term_from_expr(iter.next().unwrap(), known_vars);
-                for pair in iter.rev() {
+                let mut iter = pair.into_inner();
+                let old_pair = iter.next_back().unwrap();
+                for pair in iter {
+                    terms.push(build_term_from_expr(old_pair.clone(), known_vars));
                     known_vars.push_front(pair.as_str().to_string());
-                    terms.push(t.clone());
                 }
             }
             let t = build_term_from_expr(pair, known_vars);
@@ -94,6 +94,7 @@ fn build_term_from_expr(pair: Pair<Rule>, known_vars: &mut VecDeque<String>) -> 
     }
 }
 
+/// build commands from errorless pest's output
 fn build_command_from_expr(pair: Pair<Rule>) -> Command {
     // location to be used in a future version
     let _loc = convert_span(pair.as_span());
@@ -126,12 +127,12 @@ fn build_command_from_expr(pair: Pair<Rule>) -> Command {
     }
 }
 
-// convert pest error to kernel error
+/// convert pest error to kernel error
 fn convert_error(err: Error<Rule>) -> KernelError {
     // renaming error messages
     let err = err.renamed_rules(|rule| match *rule {
         Rule::string | Rule::Var => "variable".to_owned(),
-        Rule::number => "number".to_owned(),
+        Rule::number => "universe level".to_owned(),
         Rule::Define => "def var := term".to_owned(),
         Rule::CheckType => "check term : term".to_owned(),
         Rule::GetType => "check term".to_owned(),
@@ -142,7 +143,7 @@ fn convert_error(err: Error<Rule>) -> KernelError {
         Rule::App => "application".to_owned(),
         Rule::Prop => "Prop".to_owned(),
         Rule::Type => "Type".to_owned(),
-        _ => "".to_owned(),
+        _ => unreachable!("low level rules cannot appear in error messages"),
     });
 
     // extracting the location from the pest output
@@ -155,10 +156,10 @@ fn convert_error(err: Error<Rule>) -> KernelError {
             for c in chars {
                 i += 1;
                 if char::is_whitespace(c) {
-                    match i.cmp(&y) {
-                        Ordering::Less => left = i + 1,
-                        Ordering::Equal => left = y,
-                        Ordering::Greater => break,
+                    if i < y {
+                        left = i + 1
+                    } else {
+                        break;
                     }
                 } else {
                     right = i
@@ -170,6 +171,8 @@ fn convert_error(err: Error<Rule>) -> KernelError {
             }
             Loc::new(x, left, x, right)
         }
+        // unreachable in the current pest version but may change in the future
+        // if this is the case, this line should be used in place of an unreachable macro:
         LineColLocation::Span((x1, y1), (x2, y2)) => Loc::new(x1, y1, x2, y2),
     };
 
@@ -199,5 +202,352 @@ pub fn parse_file(file: &str) -> Result<Vec<Command>, KernelError> {
     match CommandParser::parse(Rule::file, file) {
         Ok(pairs) => Ok(pairs.into_iter().map(build_command_from_expr).collect()),
         Err(err) => Err(convert_error(err)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Command::*;
+    use super::KernelError::*;
+    use super::Term::*;
+    use super::*;
+
+    /// Error messages
+    const COMMAND_ERR: &str =
+        "expected def var := term, def var : term := term, check term : term, or check term";
+    const TERM_ERR: &str =
+        "expected variable, abstraction, dependent product, application, product, Prop, or Type";
+    const SIMPLE_TERM_ERR: &str = "expected variable, abstraction, Prop, or Type";
+    const UNIVERSE_ERR: &str = "expected universe level, variable, abstraction, Prop, or Type";
+
+    #[test]
+    fn failure_universe_level() {
+        assert_eq!(
+            parse_line("check fun x : Prop -> Type"),
+            Err(CannotParse(
+                Loc::new(1, 27, 1, 27),
+                UNIVERSE_ERR.to_string()
+            ))
+        )
+    }
+
+    #[test]
+    fn successful_definechecktype() {
+        assert_eq!(
+            parse_line("def x : Type := Prop"),
+            Ok(Define(
+                "x".to_string(),
+                Some(Type(BigUint::from(0_u64).into())),
+                Prop
+            ))
+        )
+    }
+
+    #[test]
+    fn successful_define() {
+        assert_eq!(
+            parse_line("def x := Prop"),
+            Ok(Define("x".to_string(), None, Prop))
+        )
+    }
+
+    #[test]
+    fn successful_checktype() {
+        assert_eq!(
+            parse_line("check Prop : Type"),
+            Ok(CheckType(Prop, Type(BigUint::from(0_u64).into())))
+        )
+    }
+
+    #[test]
+    fn successful_gettype_prop() {
+        assert_eq!(parse_line("check Prop"), Ok(GetType(Prop)))
+    }
+
+    #[test]
+    fn successful_var() {
+        assert_eq!(parse_line("check A"), Ok(GetType(Const("A".to_string()))));
+        assert_eq!(
+            parse_line("check fun A:Prop => A"),
+            Ok(GetType(Abs(box Prop, box Var(1.into()))))
+        )
+    }
+
+    #[test]
+    fn successful_type() {
+        assert_eq!(
+            parse_line("check Type"),
+            Ok(GetType(Type(BigUint::from(0_u64).into())))
+        );
+        assert_eq!(
+            parse_line("check Type 0"),
+            Ok(GetType(Type(BigUint::from(0_u64).into())))
+        );
+        assert_eq!(
+            parse_line("check Type 1"),
+            Ok(GetType(Type(BigUint::from(1_u64).into())))
+        )
+    }
+
+    #[test]
+    fn successful_app() {
+        assert_eq!(
+            parse_line("check A B C"),
+            Ok(GetType(App(
+                box App(box Const("A".to_string()), box Const("B".to_string())),
+                box Const("C".to_string())
+            )))
+        );
+        assert_eq!(
+            parse_line("check (A B) C"),
+            Ok(GetType(App(
+                box App(box Const("A".to_string()), box Const("B".to_string())),
+                box Const("C".to_string())
+            )))
+        );
+        assert_eq!(
+            parse_line("check A (B C)"),
+            Ok(GetType(App(
+                box Const("A".to_string()),
+                box App(box Const("B".to_string()), box Const("C".to_string()))
+            )))
+        )
+    }
+
+    #[test]
+    fn successful_prod() {
+        assert_eq!(
+            parse_line("check A -> B -> C"),
+            Ok(GetType(Prod(
+                box Const("A".to_string()),
+                box Prod(box Const("B".to_string()), box Const("C".to_string()))
+            )))
+        );
+        assert_eq!(
+            parse_line("check A -> (B -> C)"),
+            Ok(GetType(Prod(
+                box Const("A".to_string()),
+                box Prod(box Const("B".to_string()), box Const("C".to_string()))
+            )))
+        );
+        assert_eq!(
+            parse_line("check (A -> B) -> C"),
+            Ok(GetType(Prod(
+                box Prod(box Const("A".to_string()), box Const("B".to_string())),
+                box Const("C".to_string())
+            )))
+        )
+    }
+
+    #[test]
+    fn successful_dprod() {
+        assert_eq!(
+            parse_line("check (x:A) -> (y:B) -> x"),
+            Ok(GetType(Prod(
+                box Const("A".to_string()),
+                box Prod(box Const("B".to_string()), box Var(2.into()))
+            )))
+        );
+        assert_eq!(
+            parse_line("check (x:A) -> ((y:B) -> x)"),
+            Ok(GetType(Prod(
+                box Const("A".to_string()),
+                box Prod(box Const("B".to_string()), box Var(2.into()))
+            )))
+        )
+    }
+
+    #[test]
+    fn successful_abs() {
+        assert_eq!(
+            parse_line("check fun w x : Prop, y z : Prop => x"),
+            Ok(GetType(Abs(
+                box Prop,
+                box Abs(
+                    box Prop,
+                    box Abs(box Prop, box Abs(box Prop, box Var(3.into())))
+                )
+            )))
+        )
+    }
+
+    #[test]
+    fn failed_dprod() {
+        assert_eq!(
+            parse_line("check (x:A)"),
+            Err(CannotParse(
+                Loc::new(1, 7, 1, 11),
+                SIMPLE_TERM_ERR.to_string()
+            ))
+        );
+        assert_eq!(
+            parse_line("check (x:A) -> (y:B)"),
+            Err(CannotParse(
+                Loc::new(1, 16, 1, 20),
+                SIMPLE_TERM_ERR.to_string()
+            ))
+        )
+    }
+
+    #[test]
+    fn context_for_abs_args() {
+        assert_eq!(
+            parse_line("check fun x : Prop, x : x, x : x => x"),
+            Ok(GetType(Abs(
+                box Prop,
+                box Abs(
+                    box Var(1.into()),
+                    box Abs(box Var(1.into()), box Var(1.into()))
+                )
+            )))
+        );
+        assert_eq!(
+            parse_line("check fun x : Prop, x x : x => x"),
+            Ok(GetType(Abs(
+                box Prop,
+                box Abs(
+                    box Var(1.into()),
+                    box Abs(box Var(1.into()), box Var(1.into()))
+                )
+            )))
+        );
+        assert_eq!(
+            parse_line("check fun x : Prop, y z : x => z"),
+            Ok(GetType(Abs(
+                box Prop,
+                box Abs(
+                    box Var(1.into()),
+                    box Abs(box Var(2.into()), box Var(1.into()))
+                )
+            )))
+        );
+    }
+
+    #[test]
+    fn context_for_dprod_args() {
+        assert_eq!(
+            parse_line("check (x : Prop, x : x, x : x) -> x"),
+            Ok(GetType(Prod(
+                box Prop,
+                box Prod(
+                    box Var(1.into()),
+                    box Prod(box Var(1.into()), box Var(1.into()))
+                )
+            )))
+        );
+        assert_eq!(
+            parse_line("check (x : Prop, x x : x) -> x"),
+            Ok(GetType(Prod(
+                box Prop,
+                box Prod(
+                    box Var(1.into()),
+                    box Prod(box Var(1.into()), box Var(1.into()))
+                )
+            )))
+        );
+        assert_eq!(
+            parse_line("check (x : Prop, y z : x) -> z"),
+            Ok(GetType(Prod(
+                box Prop,
+                box Prod(
+                    box Var(1.into()),
+                    box Prod(box Var(2.into()), box Var(1.into()))
+                )
+            )))
+        )
+    }
+
+    #[test]
+    fn parenthesis_in_abs() {
+        assert_eq!(
+            parse_line("check fun (((w x : Prop))), y z : Prop => x"),
+            Ok(GetType(Abs(
+                box Prop,
+                box Abs(
+                    box Prop,
+                    box Abs(box Prop, box Abs(box Prop, box Var(3.into())))
+                )
+            )))
+        )
+    }
+
+    #[test]
+    fn parenthesis_in_prod() {
+        assert_eq!(
+            parse_line("check (((A))) -> (((B -> C)))"),
+            Ok(GetType(Prod(
+                box Const("A".to_string()),
+                box Prod(box Const("B".to_string()), box Const("C".to_string()))
+            )))
+        )
+    }
+
+    #[test]
+    fn parenthesis_in_dprod() {
+        assert_eq!(
+            parse_line("check (((x:A))) -> ((((y:B) -> x)))"),
+            Ok(GetType(Prod(
+                box Const("A".to_string()),
+                box Prod(box Const("B".to_string()), box Var(2.into()))
+            )))
+        )
+    }
+
+    #[test]
+    fn parenthesis_in_app() {
+        assert_eq!(
+            parse_line("check ((((((A))) (((B C))))))"),
+            Ok(GetType(App(
+                box Const("A".to_string()),
+                box App(box Const("B".to_string()), box Const("C".to_string()))
+            )))
+        )
+    }
+
+    #[test]
+    fn successful_parsers() {
+        let file = r#"
+            def x := Prop -> Prop
+
+            // this is a comment
+            check fun x:Prop => x
+        "#;
+
+        assert_eq!(
+            parse_file(file).unwrap()[0],
+            parse_line("def x := Prop -> Prop").unwrap()
+        );
+        assert_eq!(
+            parse_file(file).unwrap()[1],
+            parse_line("check fun x:Prop => x").unwrap()
+        )
+    }
+
+    #[test]
+    fn successful_convert_error() {
+        assert_eq!(
+            parse_line("chehk 2x"),
+            Err(CannotParse(Loc::new(1, 1, 1, 5), COMMAND_ERR.to_string()))
+        );
+        assert_eq!(
+            parse_line("check 2x"),
+            Err(CannotParse(Loc::new(1, 7, 1, 8), TERM_ERR.to_string()))
+        );
+        assert_eq!(
+            parse_line("check x:"),
+            Err(CannotParse(Loc::new(1, 9, 1, 9), TERM_ERR.to_string()))
+        )
+    }
+
+    #[test]
+    fn failed_parsers() {
+        assert_eq!(
+            parse_file(
+                "def x : Type := Prop -> Prop
+                 // this is a comment
+                        check .x"
+            ),
+            Err(CannotParse(Loc::new(3, 31, 3, 32), TERM_ERR.to_string()))
+        )
     }
 }
