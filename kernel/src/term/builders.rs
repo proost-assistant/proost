9dba103098c2464fb07e40d689a349f74be6ed5f
loutr <l.ta-ma@proton.me>
2022-11-17 11:57:38 +0100

    feat(builders): WIP rewriting builders

--- kernel/src/term/builders.rs
+++ kernel/src/term/builders.rs
@@ -95,13 +95,16 @@ pub mod extern_ {
         ConstNotFound(&'arena str),
     }
 
-    pub type Environment<'arena> = ImHashMap<&'arena str, (DeBruijnIndex, Term<'arena>)>;
-    pub trait Builder<'arena> =
-        FnOnce(&mut Arena<'arena>, &Environment<'arena>, DeBruijnIndex) -> ResultTerm<'arena>;
+    pub type Environment<'build, 'arena> = ImHashMap<&'build str, (DeBruijnIndex, Term<'arena>)>;
+    pub trait BuilderTrait<'build, 'arena> = FnOnce(
+        &mut Arena<'arena>,
+        &Environment<'build, 'arena>,
+        DeBruijnIndex,
+    ) -> ResultTerm<'arena>;
 
     #[inline]
-    pub fn var<'arena>(name: &'arena str) -> impl Builder<'arena> {
-        move |arena: &mut Arena<'arena>, env: &Environment<'arena>, depth| {
+    pub fn var<'build, 'arena>(name: &'build str) -> impl BuilderTrait<'build, 'arena> {
+        move |arena: &mut Arena<'arena>, env: &Environment<'build, 'arena>, depth| {
             env.get(name)
                 .map(|(bind_depth, term)| {
                     // maybe find a way to make this call efficiently lazy
@@ -110,27 +113,40 @@ pub mod extern_ {
                 })
                 .or_else(|| arena.get_binding(name))
                 .ok_or(Error {
-                    kind: DefinitionError::ConstNotFound(name).into(),
+                    kind: DefinitionError::ConstNotFound(arena.store_name(name)).into(),
                 })
         }
     }
 
     #[inline]
-    pub fn prop<'arena>() -> impl Builder<'arena> {
-        |arena: &mut Arena<'arena>, _: &Environment<'arena>, _| Ok(arena.prop())
+    pub fn prop<'build, 'arena>() -> impl BuilderTrait<'build, 'arena> {
+        |arena: &mut Arena<'arena>, _: &Environment<'build, 'arena>, _| Ok(arena.prop())
     }
 
     #[inline]
-    pub fn type_<'arena>(level: UniverseLevel) -> impl Builder<'arena> {
-        move |arena: &mut Arena<'arena>, _: &Environment<'arena>, _| Ok(arena.type_(level))
+    pub fn type_<'build, 'arena>(level: UniverseLevel) -> impl BuilderTrait<'build, 'arena> {
+        move |arena: &mut Arena<'arena>, _: &Environment<'build, 'arena>, _| Ok(arena.type_(level))
     }
 
     #[inline]
-    pub fn app<'arena, F1: Builder<'arena>, F2: Builder<'arena>>(
+    pub fn type_usize<'build, 'arena>(level: usize) -> impl BuilderTrait<'build, 'arena> {
+        use num_bigint::BigUint;
+        move |arena: &mut Arena<'arena>, _: &Environment<'build, 'arena>, _| {
+            Ok(arena.type_(BigUint::from(level).into()))
+        }
+    }
+
+    #[inline]
+    pub fn app<
+        'build,
+        'arena,
+        F1: BuilderTrait<'build, 'arena>,
+        F2: BuilderTrait<'build, 'arena>,
+    >(
         u1: F1,
         u2: F2,
-    ) -> impl Builder<'arena> {
-        |arena: &mut Arena<'arena>, env: &Environment<'arena>, depth| {
+    ) -> impl BuilderTrait<'build, 'arena> {
+        |arena: &mut Arena<'arena>, env: &Environment<'build, 'arena>, depth| {
             let u1 = u1(arena, env, depth)?;
             let u2 = u2(arena, env, depth)?;
             Ok(arena.app(u1, u2))
@@ -138,12 +154,17 @@ pub mod extern_ {
     }
 
     #[inline]
-    pub fn abs<'arena, F1: Builder<'arena>, F2: Builder<'arena>>(
-        name: &'arena str,
+    pub fn abs<
+        'build,
+        'arena,
+        F1: BuilderTrait<'build, 'arena>,
+        F2: BuilderTrait<'build, 'arena>,
+    >(
+        name: &'build str,
         arg_type: F1,
         body: F2,
-    ) -> impl Builder<'arena> {
-        move |arena: &mut Arena<'arena>, env: &Environment<'arena>, depth| {
+    ) -> impl BuilderTrait<'build, 'arena> {
+        move |arena: &mut Arena<'arena>, env: &Environment<'build, 'arena>, depth| {
             let arg_type = arg_type(arena, env, depth)?;
             let env = env.update(name, (depth, arg_type));
             let body = body(arena, &env, depth + 1.into())?;
@@ -152,16 +173,67 @@ pub mod extern_ {
     }
 
     #[inline]
-    pub fn prod<'arena, F1: Builder<'arena>, F2: Builder<'arena>>(
-        name: &'arena str,
+    pub fn prod<
+        'build,
+        'arena,
+        F1: BuilderTrait<'build, 'arena>,
+        F2: BuilderTrait<'build, 'arena>,
+    >(
+        name: &'build str,
         arg_type: F1,
         body: F2,
-    ) -> impl Builder<'arena> {
-        move |arena: &mut Arena<'arena>, env: &Environment<'arena>, depth| {
+    ) -> impl BuilderTrait<'build, 'arena> {
+        move |arena: &mut Arena<'arena>, env: &Environment<'build, 'arena>, depth| {
             let arg_type = arg_type(arena, env, depth)?;
             let env = env.update(name, (depth, arg_type));
             let body = body(arena, &env, depth + 1.into())?;
             Ok(arena.prod(arg_type, body))
+        }
+    }
+
+    pub enum Builder<'r> {
+        Var(&'r str),
+        Type(usize),
+        Prop,
+        App(&'r Builder<'r>, &'r Builder<'r>),
+        Abs(&'r str, &'r Builder<'r>, &'r Builder<'r>),
+        Prod(&'r str, &'r Builder<'r>, &'r Builder<'r>),
+    }
+
+    impl<'build> Builder<'build> {
+        pub fn realise<'arena>(&self, arena: &mut Arena<'arena>) -> ResultTerm<'arena> {
+            arena.build_from_extern(self.partial_application())
+        }
+
+        fn partial_application<'arena>(&self) -> impl BuilderTrait<'build, 'arena> + '_ {
+            |arena: &mut Arena<'arena>, env: &Environment<'build, 'arena>, depth| {
+                self.realise_in_context(arena, env, depth)
+            }
+        }
+
+        fn realise_in_context<'arena>(
+            &self,
+            arena: &mut Arena<'arena>,
+            env: &Environment<'build, 'arena>,
+            depth: DeBruijnIndex,
+        ) -> ResultTerm<'arena> {
+            use Builder::*;
+            match *self {
+                Var(s) => var(s)(arena, env, depth),
+                Type(level) => type_usize(level)(arena, env, depth),
+                Prop => prop()(arena, env, depth),
+                App(l, r) => {
+                    app(l.partial_application(), r.partial_application())(arena, env, depth)
+                }
+                Abs(s, arg, body) => {
+                    abs(s, arg.partial_application(), body.partial_application())(arena, env, depth)
+                }
+                Prod(s, arg, body) => {
+                    prod(s, arg.partial_application(), body.partial_application())(
+                        arena, env, depth,
+                    )
+                }
+            }
         }
     }
 }
