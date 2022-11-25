9bb73acb880b9897ccdb7ef0870155f61e2e4767
aalbert <augustin.albert@bleu-azure.fr>
2022-11-25 16:20:23 +0100

    apply suggestions

--- proost/src/replace_word.rs
+++ proost/src/replace_word.rs
@@ -8,11 +8,15 @@ pub fn replace_word(input: &String, from: &str, to: &str) -> String {
         if (start == 0 || input.as_bytes()[start - 1] == b' ')
             && (start + from_len == input_len || input.as_bytes()[start + from_len] == b' ')
         {
-            result.push_str(unsafe { input.get_unchecked(last_end..start) });
+            result.push_str(input.get(last_end..start).unwrap_or_else(|| unreachable!()));
             result.push_str(to);
             last_end = start + from_len;
         }
     }
-    result.push_str(unsafe { input.get_unchecked(last_end..input_len) });
+    result.push_str(
+        input
+            .get(last_end..input_len)
+            .unwrap_or_else(|| unreachable!()),
+    );
     result
 }
