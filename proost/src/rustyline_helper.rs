1a0f040173f43cdbc4702e53e13f6131064a6609
aalbert <augustin.albert@bleu-azure.fr>
2022-11-26 14:32:52 +0100

    format

--- proost/src/rustyline_helper.rs
+++ proost/src/rustyline_helper.rs
@@ -138,9 +138,9 @@ impl Highlighter for RustyLineHelper {
                 let s = String::from(matching as char);
                 copy.replace_range(pos..=pos, &s.blue().bold());
             }
-        KEYWORDS.iter().for_each(|keyword| {
-            replace_inplace(&mut copy, keyword, &keyword.blue().bold())
-        });
+        KEYWORDS
+            .iter()
+            .for_each(|keyword| replace_inplace(&mut copy, keyword, &keyword.blue().bold()));
         Owned(copy)
     }
 }
