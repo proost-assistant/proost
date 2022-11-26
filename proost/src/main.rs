1f0aa51edcf290c723e753ff12d98446ca9d076d
aalbert <augustin.albert@bleu-azure.fr>
2022-11-26 14:39:46 +0100

    Resolve "Add functionnalities to proost (toplevel)" ✨️ Closes #39 🐔️👍️ Approved-by: aalbert <augustin.albert@bleu-azure.fr> Approved-by: v-lafeychine <vincent.lafeychine@proton.me> Approved-by: belazy <aarthuur01@gmail.com>
    🦀️🍰🦀️🍰🦀️🍰
    
    * format
    
    * apply suggestion
    
    * clippy
    
    * apply suggestions
    
    * Apply 1 suggestion(s) to 1 file(s)
    
    * Apply 1 suggestion(s) to 1 file(s)
    
    * Apply 1 suggestion(s) to 1 file(s)
    
    * Apply 1 suggestion(s) to 1 file(s)
    
    * Apply 1 suggestion(s) to 1 file(s)
    
    * Apply 1 suggestion(s) to 1 file(s)
    
    * apply suggested change
    
    * apply suggestion
    
    * apply suggestions
    
    * add cmd option description
    
    * check if terminal and add option for syntax coloring
    
    * add tab
    
    * clippy fix
    
    * apply suggestion
    
    * fix coloring
    
    * apply suggestions
    
    * apply suggestions
    
    * Apply 1 suggestion(s) to 1 file(s)
    
    * add everything listed in issue except full multiline editing

--- proost/src/main.rs
+++ proost/src/main.rs
@@ -1,29 +1,37 @@
 #![feature(box_syntax)]
+#![feature(let_chains)]
 
 mod process;
+mod rustyline_helper;
 
-use clap::Parser;
-use kernel::Environment;
-use process::*;
-use rustyline::error::ReadlineError;
-use rustyline::Editor;
-use std::error::Error;
 use std::fs;
 
-// clap configuration
+use atty::Stream;
+use clap::Parser;
+use rustyline::error::ReadlineError;
+use rustyline::{Cmd, Editor, EventHandler, KeyCode, KeyEvent, Modifiers, Result};
+use rustyline_helper::*;
+
+use kernel::Environment;
+use process::*;
+
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    /// some .mdln files
     files: Vec<String>,
+    /// remove syntax highlighting
+    #[arg(long)]
+    no_color: bool,
 }
 
-// constants fetching
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const NAME: &str = env!("CARGO_PKG_NAME");
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     let args = Args::parse();
 
+    // check if files are inputed
     if !args.files.is_empty() {
         for path in args.files.iter() {
             match fs::read_to_string(path) {
@@ -36,8 +44,22 @@ fn main() -> Result<(), Box<dyn Error>> {
         return Ok(());
     }
 
-    let mut rl_err: Option<ReadlineError> = None;
-    let mut rl = Editor::<()>::new()?;
+    // check if we are in a terminal
+    if atty::isnt(Stream::Stdout) || atty::isnt(Stream::Stdin) {
+        return Ok(());
+    }
+
+    let helper = RustyLineHelper::new(!args.no_color);
+    let mut rl = Editor::<RustyLineHelper>::new()?;
+    rl.set_helper(Some(helper));
+    rl.bind_sequence(
+        KeyEvent::from('\t'),
+        EventHandler::Conditional(Box::new(TabEventHandler)),
+    );
+    rl.bind_sequence(
+        KeyEvent(KeyCode::Enter, Modifiers::ALT),
+        EventHandler::Simple(Cmd::Newline),
+    );
     println!("Welcome to {} {}", NAME, VERSION);
 
     let mut env = Environment::new();
@@ -52,14 +74,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             Ok(_) => (),
             Err(ReadlineError::Interrupted) => {}
             Err(ReadlineError::Eof) => break,
-            Err(err) => {
-                rl_err = Some(err);
-                break;
-            }
+            Err(err) => return Err(err),
         }
     }
-    match rl_err {
-        None => Ok(()),
-        Some(err) => Err(box err),
-    }
+    Ok(())
 }
