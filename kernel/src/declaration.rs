c9cd226b0e64b180be555d8a8f1b2b55c0c52584
arthur-adjedj <arthur.adjedj@gmail.com>
2022-12-01 14:07:06 +0100

    first commit

---
+++ kernel/src/declaration.rs
@@ -0,0 +1,58 @@
+use crate::term::Term;
+use crate::universe::UniverseLevel;
+use crate::KernelError;
+use crate::Level;
+
+/// Declarations constructed through commands. A declaration describes a constant in the environment, whether it's a definition with
+/// a corresponding term, or an axiom with only a type.
+/// univ_vars corresponds to the number of universe variables bound to the declaration.
+/// No universe variable can be "free" in a term, meaning that for all Var(i) in ty or term, i<univ_vars.
+/// Additionally, ty and term *should* in theory always have the same number of universe variables, and as such, only a single method is needed.
+/// However, additional checks to ensure this invariant will have to be put in place. For now, when constructing declarations, only the number of
+/// universes in ty are counted.
+#[derive(Clone, Debug, PartialEq, Eq)]
+ pub struct Declaration {
+    ty: Term,
+    term: Option<Term>,
+    univ_vars: usize,
+}
+
+impl Declaration {
+    pub fn make(term: Option<Term>, ty: Term) -> Declaration {
+        Declaration {
+            ty: ty.clone(),
+            term,
+            univ_vars: ty.univ_vars(),
+        }
+    }
+
+    /// Returns the type linked to a definition in a given environment.
+    pub fn get_type(&self, vec: &[UniverseLevel]) -> Result<Term, KernelError> {
+        if self.univ_vars != vec.len() {
+            Err(KernelError::WrongNumberOfUniverseArguments(
+                self.univ_vars,
+                vec.len(),
+            ))
+        } else {
+            Ok(self.ty.substitute_univs(vec))
+        }
+    }
+
+    /// Returns the type linked to a definition in a given environment.
+    pub fn get_type_free_univ(&self) -> Term {
+        self.ty.clone()
+    }
+
+    /// Returns the term linked to a definition in a given environment.
+    /// Since the declaration might be an axiom, it might not have an associated term to reduce to, hence the Option.
+    pub fn get_term(&self, vec: &[UniverseLevel]) -> Result<Option<Term>, KernelError> {
+        if self.univ_vars != vec.len() {
+            Err(KernelError::WrongNumberOfUniverseArguments(
+                self.univ_vars,
+                vec.len(),
+            ))
+        } else {
+            Ok(self.clone().term.map(|x| x.substitute_univs(vec)))
+        }
+    }
+}
---
+++ kernel/src/environment.rs
@@ -0,0 +1,60 @@
+use crate::declaration::Declaration;
+use crate::error::KernelError;
+use crate::term::Term;
+use crate::universe::UniverseLevel;
+use derive_more::From;
+use std::collections::{hash_map, HashMap};
+
+/// Global Environment, contains the term and type of every definitions, denoted by their strings.
+#[derive(Clone, Default, Debug, From, PartialEq, Eq)]
+pub struct Environment(HashMap<String, Declaration>);
+
+impl Environment {
+    /// Creates an empty environment.
+    pub fn new() -> Self {
+        Self::default()
+    } 
+
+    pub fn insert(&mut self, s: String, decl: Declaration) -> Result<&Self, KernelError> {
+        if let hash_map::Entry::Vacant(e) = self.0.entry(s.clone()) {
+            e.insert(decl);
+            Ok(self)
+        } else {
+            Err(KernelError::AlreadyDefined(s))
+        }
+    }
+
+    /// Creates a new environment binding s with t1 : t2
+    pub fn insert_def(&mut self, s: String, t1: Term, t2: Term) -> Result<&Self, KernelError> {
+        self.insert(s, Declaration::make(Some(t1), t2))
+    }
+
+    /// Creates a new environment binding s with an axiom of type ty
+    pub fn insert_axiom(&mut self, s: String, ty: Term) -> Result<&Self, KernelError> {
+        self.insert(s, Declaration::make(None, ty))
+    }
+
+    /// Returns the term linked to a definition in a given environment.
+    pub fn get_term(&self, s: &String, vec: &[UniverseLevel]) -> Result<Option<Term>, KernelError> {
+        match self.0.get(s) {
+            Some(decl) => Ok(decl.get_term(vec)?),
+            None => Err(KernelError::ConstNotFound(s.clone())),
+        }
+    }
+
+    /// Returns the type linked to a definition in a given environment.
+    pub fn get_type(&self, s: &String, vec: &[UniverseLevel]) -> Result<Term, KernelError> {
+        match self.0.get(s) {
+            Some(decl) => Ok(decl.get_type(vec)?),
+            None => Err(KernelError::ConstNotFound(s.clone())),
+        }
+    }
+
+    /// Returns the type linked to a definition in a given environment.
+    pub fn get_type_free_univ(&self, s: &String) -> Result<Term, KernelError> {
+        match self.0.get(s) {
+            Some(decl) => Ok(decl.get_type_free_univ()),
+            None => Err(KernelError::ConstNotFound(s.clone())),
+        }
+    }
+}
