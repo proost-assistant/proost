603d404cb8305ddaa3b458c5ab4c88b3b777829b
arthur-adjedj <arthur.adjedj@gmail.com>
2022-12-14 11:38:13 +0100

    feat: uwu

--- kernel/src/type_checker.rs
+++ kernel/src/type_checker.rs
@@ -46,36 +46,39 @@ impl<'arena> Arena<'arena> {
     /// The conversion is untyped, meaning that it should **only** be called during type-checking
     /// when the two `Term`s are already known to be of the same type and in the same context.
     fn conversion(&mut self, lhs: Term<'arena>, rhs: Term<'arena>) -> bool {
-        lhs == rhs
-            || self.whnf(lhs) == self.whnf(rhs)
-            || match (&*self.whnf(lhs), &*self.whnf(rhs)) {
-                (Prop, Prop) => true,
+        lhs == rhs || {
+            let lhs = self.whnf(lhs);
+            let rhs = self.whnf(rhs);
+            lhs == rhs
+                || match (&*lhs, &*rhs) {
+                    (Prop, Prop) => true,
 
-                (Type(i), Type(j)) => *i == *j,
+                    (Type(i), Type(j)) => *i == *j,
 
-                (Var(i, _), Var(j, _)) => i == j,
+                    (Var(i, _), Var(j, _)) => i == j,
 
-                (&Prod(t1, u1), &Prod(t2, u2)) => self.conversion(t1, t2) && self.conversion(u1, u2),
+                    (&Prod(t1, u1), &Prod(t2, u2)) => self.conversion(t1, t2) && self.conversion(u1, u2),
 
-                // Since we assume that both values already have the same type,
-                // checking conversion over the argument type is useless.
-                // However, this doesn't mean we can simply remove the arg type
-                // from the type constructor in the enum, it is needed to quote back to terms.
-                (&Abs(_, t), &Abs(_, u)) => self.conversion(t, u),
+                    // Since we assume that both values already have the same type,
+                    // checking conversion over the argument type is useless.
+                    // However, this doesn't mean we can simply remove the arg type
+                    // from the type constructor in the enum, it is needed to quote back to terms.
+                    (&Abs(_, t), &Abs(_, u)) => self.conversion(t, u),
 
-                (&App(t1, u1), &App(t2, u2)) => self.conversion(t1, t2) && self.conversion(u1, u2),
+                    (&App(t1, u1), &App(t2, u2)) => self.conversion(t1, t2) && self.conversion(u1, u2),
 
-                (&App(t1, t2), _) | (_, &App(t1, t2)) => match &*self.whnf(t1) {
-                    &Abs(_, t1) => {
-                        let subst = self.substitute(t1, t2, 1);
-                        self.conversion(subst, rhs.to_owned())
+                    (&App(t1, t2), _) | (_, &App(t1, t2)) => match &*self.whnf(t1) {
+                        &Abs(_, t1) => {
+                            let subst = self.substitute(t1, t2, 1);
+                            self.conversion(subst, rhs.to_owned())
+                        },
+
+                        _ => false,
                     },
 
                     _ => false,
-                },
-
-                _ => false,
-            }
+                }
+        }
     }
 
     /// Checks whether two terms are definitionally equal.
