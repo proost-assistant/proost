3bcfd0f98941609c1386ae4056e8768555248a22
arthur-adjedj <arthur.adjedj@gmail.com>
2022-12-19 15:23:21 +0100

    chore(kernel): documentation of `level_builder.rs`

--- kernel/src/memory/level/builder.rs
+++ kernel/src/memory/level/builder.rs
@@ -1,17 +1,16 @@
-//! Collection of safe functions to build Terms
+//! Collection of safe functions to build Levels
 //!
-//! This module provides two main ways of building terms. The first one is via closures: users can
-//! manipulate closures and create bigger ones which, when [built](Arena::build), provide the expected
-//! term.
+//! This module provides two main ways of building levels. The first one is via closures: users can
+//! manipulate closures and create bigger ones which, when [built](Arena::build_level), provide the expected
+//! level.
 //!
 //! The overall syntax remains transparent to the user. This means the user focuses on the
-//! structure of the term they want to build, while the [closures](`BuilderTrait`) internally build an appropriate
-//! logic: converting regular terms into de Bruijn-compatible ones, assigning types to variables,
-//! etc.
+//! structure of the level they want to build, while the [closures](`LevelBuilderTrait`) internally build an appropriate
+//! logic: converting regular universe names into their corresponding variable numbers
 //!
 //! The other way to proceed is built on top of the latter. Users can also manipulate a sort of
-//! *high-level term* or *template*, described by the public enumeration [`Builder`], and at any
-//! moment, [realise](Builder::realise) it.
+//! *high-level level* or *template*, described by the public enumeration [`Builder`], and at any
+//! moment, [realise](LevelBuilder::realise) it.
 
 use derive_more::Display;
 use std::collections::HashMap;
@@ -31,11 +30,10 @@ pub enum LevelError<'arena> {
 /// (depth at which they were bound, their type)
 pub type Environment<'build> = HashMap<&'build str, usize>;
 
-/// The trait of closures which build terms with an adequate logic.
+/// The trait of closures which build levels with an adequate logic.
 ///
-/// A call with a triplet of arguments `(arena, env, index)` of a closure with this trait should
-/// build a definite term in the [`Arena`] `arena`, knowing the bindings declared in `environment`,
-/// provided that the term is built at a current depth `index`.
+/// A call with a couple of arguments `(arena, env)` of a closure with this trait should
+/// build a definite level in the [`Arena`] `arena`.
 ///
 /// Please note that this is just a trait alias, meaning it enforces very little constraints: while
 /// functions in this module returning a closure with this trait are guaranteed to be sound, end
@@ -43,14 +41,14 @@ pub type Environment<'build> = HashMap<&'build str, usize>;
 pub trait BuilderTrait<'build> = for<'arena> FnOnce(&mut Arena<'arena>, &Environment<'build>) -> ResultLevel<'arena>;
 
 impl<'arena> Arena<'arena> {
-    /// Returns the term built from the given closure, provided with an empty context, at depth 0.
+    /// Returns the level built from the given closure, provided with a Level environment, which binds names to `usize`s
     #[inline]
     pub fn build_level<'build, F: BuilderTrait<'build>>(&mut self, f: F) -> ResultLevel<'arena> {
         f(self, &Environment::new())
     }
 }
 
-/// Returns a closure building a variable associated to the name `name`
+/// Returns a closure building a universe variable associated to the name `name`
 #[inline]
 pub const fn var(name: &str) -> impl BuilderTrait<'_> {
     move |arena, env| {
@@ -71,8 +69,7 @@ pub const fn const_<'build>(n: usize) -> impl BuilderTrait<'build> {
     move |arena, _| Ok(Level::from(n, arena))
 }
 
-/// Returns a closure building the application of two terms built from the given closures `u1` and
-/// `u2`.
+/// Returns a closure building the successor of a level built from the given closure `u1`
 #[inline]
 #[no_coverage]
 pub const fn succ<'build, F1: BuilderTrait<'build>>(u1: F1) -> impl BuilderTrait<'build> {
@@ -82,7 +79,7 @@ pub const fn succ<'build, F1: BuilderTrait<'build>>(u1: F1) -> impl BuilderTrait
     }
 }
 
-/// Returns a closure building the application of two terms built from the given closures `u1` and
+/// Returns a closure building the max of two levels built from the given closures `u1` and
 /// `u2`.
 #[inline]
 #[no_coverage]
@@ -94,6 +91,8 @@ pub const fn max<'build, F1: BuilderTrait<'build>, F2: BuilderTrait<'build>>(u1:
     }
 }
 
+/// Returns a closure building the imax of two levels built from the given closures `u1` and
+/// `u2`.
 #[inline]
 #[no_coverage]
 pub const fn imax<'build, F1: BuilderTrait<'build>, F2: BuilderTrait<'build>>(u1: F1, u2: F2) -> impl BuilderTrait<'build> {
@@ -104,12 +103,11 @@ pub const fn imax<'build, F1: BuilderTrait<'build>, F2: BuilderTrait<'build>>(u1
     }
 }
 
-/// Template of terms.
+/// Template of levels.
 ///
-/// A Builder describes a term in a naive but easy to build manner. It strongly resembles the
-/// [payload](`crate::term::arena::Payload`) type, except that `Var`, `Abs` and `Prod` constructors
-/// include a name, as in the classic way of writing lambda-terms (i.e. no de Bruijn indices
-/// involved).
+/// A LevelBuilder describes a level in a naive but easy to build manner. It strongly resembles the
+/// [level](`crate::memory::level::Level`) type, except that the `Var` constructor
+/// include a name, as in the syntactic way of writing levels.
 #[derive(Clone, Debug, Display, PartialEq, Eq)]
 pub enum Builder<'builder> {
     #[display(fmt = "0")]
@@ -130,8 +128,8 @@ pub enum Builder<'builder> {
 }
 
 impl<'build> Builder<'build> {
-    /// Build a terms from a [`Builder`]. This internally uses functions described in the
-    /// [builders](`crate::term::builders`) module.
+    /// Build a levels from a [`Builder`]. This internally uses functions described in the
+    /// [builders](`crate::level::builder`) module.
     pub fn realise<'arena>(&self, arena: &mut Arena<'arena>) -> ResultLevel<'arena> {
         arena.build_level(self.partial_application())
     }
