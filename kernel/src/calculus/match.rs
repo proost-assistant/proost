26eee597f34571d8b68c507ac48a300a3f363f87
arthur-adjedj <arthur.adjedj@gmail.com>
2023-02-21 14:55:12 +0100

    doc : todo on matches

---
+++ kernel/src/calculus/match.rs
@@ -0,0 +1,7 @@
+// TODO
+// - Add reduction on pattern-matches
+// - Add pretty-printing of patterns
+// - Add the corresponding correspondance between constructors and terms to the arena
+// - Extend the builders to make use of this
+// - Add an exhaustiveness checker
+// - Extend the syntax accordingly (design decision to answer. ex: how to handle the empty match ?)
